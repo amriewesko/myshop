010468b68718b23f3bfa1f4badd41c0ba471b629
amriewesko <106768013@yru.ac.th>
2025-07-01 23:42:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 // !!! Google Apps Script URLs (Deployed Web App URLs) !!!
 // IMPORTANT: REPLACE THESE PLACEHOLDERS WITH YOUR ACTUAL DEPLOYED GOOGLE APPS SCRIPT URLs
 // PUBLIC_APPS_SCRIPT_URL: สำหรับการอ่านข้อมูล (doGet) - Deploy ด้วย "Who has access: Anyone"
-const PUBLIC_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwNUY7e-YxY5LlWIiJQUmr3jqFU8u6V8lFOZCFmkMGRTwM_6Vcdmid3KS2VYeGMHAJX/exec'; 
+const PUBLIC_APPS_SCRIPT_URL = 'https://script.google.com/a/macros/yru.ac.th/s/AKfycbyT356UjNGqqJt4TJOi0UlWNRQZX8CFymtYSjcPAY1K1Wi2joHlr6R-UF4mTl0RHo1V/exec'; 
 
 // ADMIN_APPS_SCRIPT_URL: สำหรับการเขียนข้อมูล (doPost) - Deploy ด้วย "Who has access: Only myself"
-const ADMIN_APPS_SCRIPT_URL = 'https://script.google.com/a/macros/yru.ac.th/s/AKfycbw-EqlboNzR_cw4cFe3K_YbvCgsnj1rBKdKiv3OH5A7bAEcabcrDNi0YacNXGwW-6gV/exec'; // *** สำคัญ: ต้องแทนที่ด้วย URL ของ Admin Web App ของคุณ ***
+const ADMIN_APPS_SCRIPT_URL = 'https://script.google.com/a/macros/yru.ac.th/s/AKfycbwW7g-uCN0R5NmgtbjgrmD5Yk-BxFpgJajS28Ll7J1FWmFjhW3rlvxr2ulC6WQ7e94c/exec'; // *** สำคัญ: ต้องแทนที่ด้วย URL ของ Admin Web App ของคุณ ***
 
 // ADMIN_SECRET_KEY ถูกลบออกทั้งหมดเพื่อความปลอดภัยสูงสุด
 // การยืนยันตัวตนสำหรับหน้า Admin จะถูกจัดการโดย Google Account ของคุณเองเมื่อ Deploy Apps Script
